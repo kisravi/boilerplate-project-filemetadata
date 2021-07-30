df8b3d0c4bb5b3070dd2085c492dfaf78eea35f8
kishan ravi <87862580+kisravi@users.noreply.github.com>
2021-07-30 07:22:13 +0000

    Added API '/api/fileanalyse' to Upload file

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
 require('dotenv').config()
-
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
